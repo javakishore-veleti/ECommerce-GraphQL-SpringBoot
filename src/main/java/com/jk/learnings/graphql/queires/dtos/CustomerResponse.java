a329960e299d0c7879620675e21f2465c5b95dc3
Kishore <>
2025-03-22 19:43:32 -0400

    Added graphql-java-tools maven dependency

---
+++ src/main/java/com/jk/learnings/graphql/queires/dtos/CustomerResponse.java
@@ -0,0 +1,115 @@
+package com.jk.learnings.graphql.queires.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("ALL")
+@Getter
+@Setter
+public class CustomerResponse {
+    private String id;
+    private String name;
+    private String customerType;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String mobilePhone;
+    private String contactPhone;
+    private String comments;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerResponse{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", customerType='" + customerType + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", mobilePhone='" + mobilePhone + '\'' +
+                ", contactPhone='" + contactPhone + '\'' +
+                ", comments='";
+    }
+}
---
+++ src/main/java/com/jk/learnings/graphql/services/resolvers/impl/CustomerRespResolver.java
@@ -0,0 +1,53 @@
+package com.jk.learnings.graphql.services.resolvers.impl;
+
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.jk.learnings.graphql.queires.dtos.CustomerResponse;
+import org.springframework.stereotype.Service;
+
+@SuppressWarnings("ALL")
+@Service
+public class CustomerRespResolver implements GraphQLResolver<CustomerResponse> {
+
+    public String getId(CustomerResponse customerResponse) {
+        return customerResponse.getId();
+    }
+
+    public String getName(CustomerResponse customerResponse) {
+        return customerResponse.getName();
+    }
+
+    public String getCustomerType(CustomerResponse customerResponse) {
+        return customerResponse.getCustomerType();
+    }
+
+    public String getFirstName(CustomerResponse customerResponse) {
+        return customerResponse.getFirstName();
+    }
+
+    public String getLastName(CustomerResponse customerResponse) {
+        return customerResponse.getLastName();
+    }
+
+    public String getEmail(CustomerResponse customerResponse) {
+        return customerResponse.getEmail();
+    }
+
+    public String getPhone(CustomerResponse customerResponse) {
+        return customerResponse.getPhone();
+    }
+
+    public String getMobilePhone(CustomerResponse customerResponse) {
+        return customerResponse.getMobilePhone();
+    }
+
+    public String getContactPhone(CustomerResponse customerResponse) {
+        return customerResponse.getContactPhone();
+    }
+
+    public String getComments(CustomerResponse customerResponse) {
+        return customerResponse.getComments();
+    }
+
+    // Add any other methods you need to resolve fields in CustomerResponse
+}
+
